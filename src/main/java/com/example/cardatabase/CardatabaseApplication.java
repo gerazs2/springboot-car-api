d92478f05d0939fbd4a2a52ea125fc032b0ef012
jgmagadan <gerardo.magadan@gmail.com>
2024-12-30 23:04:07 -0600

    first commit

---
+++ src/main/java/com/example/cardatabase/CardatabaseApplication.java
@@ -0,0 +1,55 @@
+package com.example.cardatabase;
+
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.cardatabase.domain.Car;
+import com.example.cardatabase.domain.CarRepository;
+import com.example.cardatabase.domain.Owner;
+import com.example.cardatabase.domain.OwnerRepository;
+
+
+@SpringBootApplication
+public class CardatabaseApplication implements CommandLineRunner {
+	private static final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
+
+	private final CarRepository repository;
+	private final OwnerRepository orepository;
+
+
+
+	public CardatabaseApplication(CarRepository repository, OwnerRepository orepository) {
+		this.repository = repository;
+		this.orepository = orepository;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardatabaseApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// Add owner objects and save these to db
+		Owner owner1 = new Owner("Jason" , "Hein");
+		Owner owner2 = new Owner("Gerardo" , "Magadan");
+		Owner owner3 = new Owner("Joe" , "Triviani");
+
+		orepository.saveAll(Arrays.asList(owner1, owner2,owner3));
+			
+		repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2023, 59000, owner1));
+		repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2020, 29000, owner2));
+		repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2022, 39000, owner2));
+		repository.save(new Car("Chevrolet", "Silverado", "Green", "ZPO-1645", 2024, 79000, owner3));
+
+		
+		// Fetch all cars and log to console
+		for (Car car : repository.findAll()) {
+			logger.info("brand: {}, model: {}", car.getBrand(), car.getModel());
+		}	
+	}
+}
---
+++ src/main/java/com/example/cardatabase/domain/CarRepository.java
@@ -0,0 +1,9 @@
+package com.example.cardatabase.domain;
+
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+//@RepositoryRestResource(path = "vehicles")
+public interface CarRepository extends CrudRepository<Car, Long> {
+	
+}
