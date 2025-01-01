c0c11b7a23916dff1dac4c5678169f806cb60cf0
jgmagadan <gerardo.magadan@gmail.com>
2025-01-01 14:10:46 -0600

    Adding Cors

--- src/main/java/com/example/cardatabase/SecurityConfig.java
+++ src/main/java/com/example/cardatabase/SecurityConfig.java
@@ -16,6 +16,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import java.util.Arrays;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 import com.example.cardatabase.service.UserDetailsServiceImpl;
 
@@ -50,19 +55,29 @@ return authConfig.getAuthenticationManager();
 }
 
 @Bean
-public SecurityFilterChain filterChain(HttpSecurity http) throws
- Exception {
-		http.csrf((csrf) -> csrf.disable())
-		 .sessionManagement((sessionManagement) ->
-		 sessionManagement.sessionCreationPolicy(
-		 SessionCreationPolicy.STATELESS))
-		 .authorizeHttpRequests((authorizeHttpRequests) ->
-		 authorizeHttpRequests.requestMatchers(HttpMethod.POST,
-		 "/login").permitAll().anyRequest().authenticated())
-		 .addFilterBefore(authenticationFilter,
-		 UsernamePasswordAuthenticationFilter.class)
-		 .exceptionHandling((exceptionHandling) -> exceptionHandling.
-		 authenticationEntryPoint(exceptionHandler));
- return http.build();
+public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	http.csrf((csrf) -> csrf.disable()).cors(withDefaults())
+			.sessionManagement(
+					(sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+					.requestMatchers(HttpMethod.POST, "/login").permitAll().anyRequest().authenticated())
+			.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+			.exceptionHandling((exceptionHandling) -> exceptionHandling.authenticationEntryPoint(exceptionHandler));
+	return http.build();
+}
+
+@Bean
+public CorsConfigurationSource corsConfigurationSource() {
+	UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	CorsConfiguration config = new CorsConfiguration();
+	config.setAllowedOrigins(Arrays.asList("*"));
+	config.setAllowedMethods(Arrays.asList("*"));
+	// localhost:3000 is allowed
+	//config.setAllowedOrigins(Arrays.asList ("http://localhost:3000"));
+	config.setAllowedHeaders(Arrays.asList("*"));
+	config.setAllowCredentials(false);
+	config.applyPermitDefaultValues();
+	source.registerCorsConfiguration("/**", config);
+	return source;
 }
 }
